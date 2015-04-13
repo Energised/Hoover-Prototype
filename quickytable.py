ba28ac1d76ce1ad255ea6ba9fa6187a416ce9d5d
Daniel Woolsey <woolsey64@gmail.com>
2015-04-13 13:35:54 +0100

    Update quickytable.py

--- quickytable.py
+++ quickytable.py
@@ -5,13 +5,13 @@
 # makes my life just a little easier
 #
 
-from prettytable import PrettyTable
+from prettytable import PrettyTable # uses the PrettyTable lib
 
-class QuickyTable(PrettyTable):
+class QuickyTable(PrettyTable): # class inheriting from PrettyTable
 	
-	def __init__(self, *args):
-		self.args = args
-		self.table = PrettyTable(['Number','Option'])
-		for number, place in enumerate(self.args):
+	def __init__(self, *args): # unpacks a user defined list
+		self.args = args # defines list
+		self.table = PrettyTable(['Number','Option']) # creates a PrettyTable instance
+		for number, place in enumerate(self.args): # 
 			self.table.add_row([number, place])
 		print self.table.get_string()
